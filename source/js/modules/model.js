a5342daf41694d64bc15a8859d23cb951f2eeb9a
Anton Kiyan <kiyananton@gmail.com>
2020-10-23 23:11:35 +0300

    Новые изменения

--- source/js/modules/model.js
+++ source/js/modules/model.js
@@ -1,4 +1,6 @@
 (function () {
+  const modelLinks = document.querySelectorAll('.model__link')
+
   const initSwiper = () => {
     return new Swiper('.model__image-container', {
       pagination: {
@@ -9,6 +11,16 @@
       loop: true,
     });
   };
+
+  modelLinks.forEach((link) => {
+    link.addEventListener('click', (evt) => {
+
+      console.log(evt.target.tagName);
+      if (evt.target.tagName.toLowerCase() === 'img') {
+        evt.preventDefault();
+      }
+    })
+  });
 
   initSwiper();
 })();
